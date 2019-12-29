27ee26d57d54a30692036262be91dfd293642fea
jomosquera <david.es1005@gmail.com>
2019-12-29 16:26:32 -0500

    Ralizar pedidos de mmultiples prendas

--- src/main/java/io/jomosquera/personalsoft/empresaLeonisa/Pedido.java
+++ src/main/java/io/jomosquera/personalsoft/empresaLeonisa/Pedido.java
@@ -1,5 +1,29 @@
 package io.jomosquera.personalsoft.empresaLeonisa;
 
+import java.util.Vector;
+
 public class Pedido {
+    private int cantidadUnidad;
+
+    Vector<Object> pedidos = new Vector<Object>();
+
+    public Pedido() {
+    }
+
+    public Pedido(int cantidadUnidad) {
+        this.cantidadUnidad = cantidadUnidad;
+    }
+
+    public void asignarProducto(Leonisa leonisa){
+        pedidos.add(leonisa);
+    }
+
+    public void asignarProducto(Leo leo){
+        pedidos.add(leo);
+    }
+
+    public void asignarProducto(Teen teen){
+        pedidos.add(teen);
+    }
 
 }
---
+++ src/test/java/io/jomosquera/personalsoft/empresaLeonisa/PedidoTest.java
@@ -0,0 +1,54 @@
+package io.jomosquera.personalsoft.empresaLeonisa;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class PedidoTest {
+
+    private Pedido pedidosLeonisa;
+    private Pedido pedidosLeo;
+    private Pedido pedidosTeen;
+    private Leonisa leonisa;
+    private Leo leo;
+    private Teen teen;
+
+    private ArrayList<Object> pedidos = new ArrayList<Object>();
+
+    @Before
+    public void setUp() throws Exception {
+
+        pedidosLeonisa = new Pedido(10);
+        pedidosLeo = new Pedido(3);
+        pedidosTeen = new Pedido(50);
+
+        leonisa = new Leonisa("1234EF", 35000, "Boxer", "S", "Rojo", "Terciopelo");
+        leo = new Leo("RUC1234", 15000, "Camiseta", "S", "Azul", "Terciopelo");
+        teen = new Teen("AFB00123", 25000, "Bra", "34C", "Amarillo", "Terciopelo");
+
+        pedidos.add(leonisa);
+        pedidos.add(leo);
+        pedidos.add(teen);
+    }
+
+    @Test
+    public void asignarProductoLeonisaTest() {
+        boolean asignadoLeonisa = pedidos.contains(leonisa);
+        Assert.assertTrue("No se ha asignado un producto 'Leonisa' al pedido", asignadoLeonisa);
+    }
+
+    @Test
+    public void AsignarProductoLeoTest() {
+        boolean asignadoLeo = pedidos.contains(leo);
+        Assert.assertTrue("No se ha asignado un producto 'Leo' al pedido", asignadoLeo);
+    }
+
+    @Test
+    public void AsignarProductoTeenTest() {
+        boolean asignadoTeen = pedidos.contains(teen);
+        Assert.assertTrue("No se ha asignado un producto 'Teen' al pedido", asignadoTeen);
+    }
+}
